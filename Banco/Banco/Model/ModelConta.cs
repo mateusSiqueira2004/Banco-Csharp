4e405a297f57e4eadd64c9c6798867448ec7be7f
mateusSSalomao <118471492+mateusSiqueira2004@users.noreply.github.com>
2023-09-01 11:12:06 -0300

    atualizando

---
+++ Banco/Banco/Model/ModelConta.cs
@@ -0,0 +1,89 @@
+﻿namespace Banco.Model
+{
+    internal class ModelConta
+    {
+        private int numero, agencia, tipo;
+        private string titular = string.Empty;
+        private decimal saldo;
+
+        public ModelConta(int numero, int agencia, int tipo, string titular, decimal saldo)
+        {
+            this.numero = numero;
+            this.agencia = agencia;
+            this.tipo = tipo;
+            this.titular = titular;
+            this.saldo = saldo;
+        }
+        public int getNumero() { 
+            return numero;
+        }
+        public int getAgencia(){
+            return agencia; 
+        }
+        public int getTipo(){
+            return tipo; 
+        }
+        public string getTitular() {  
+            return titular; 
+        }
+        public decimal getSaldo() {  
+            return saldo; 
+        }
+
+        public void setSaldo(decimal saldo) { 
+            this.saldo = saldo; 
+        }
+        public void setTitular(string titular) { 
+            this.titular = titular; 
+        }
+        public void setTipo(int tipo) { 
+            this.tipo = tipo; 
+        }
+        public void setAgencia(int agencia) { 
+            this.agencia = agencia; 
+        }
+        public void setNumero(int numero) { 
+            this.numero = numero; 
+        }
+
+        public bool resulSaque(decimal valor)
+        {
+            if (this.saldo < valor) {
+                Console.WriteLine("Saldo insuficiente");
+                return false;
+            }
+            this.setSaldo(this.saldo - valor);
+            return true;
+        }
+        public void Depositar(decimal valor)
+        {
+            this.setSaldo(this.saldo + valor);
+        }
+
+        public void Visualizar()
+        {
+            string tipo = string.Empty;
+            
+            switch(this.tipo)
+            {
+                case 1:
+                    tipo = "Conta Corrente";
+                    break;
+                case 2:
+                    tipo = "Conta Poupança";
+                    break;
+            }
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(
+                "\n*************************************************\n" +
+                "\n     AQUI ESTÁ INFORMAÇÕES DA SUA CONTA\n" +
+                "\n*************************************************\n" +
+                $"\nSeu Numero de usuario: {this.numero}" +
+                $"\nSua agência da sua conta: {this.agencia}" +
+                $"\nSeu tipo de conta: {tipo}" +
+                $"\nNome registrado na sua conta: {this.titular}" +
+                $"\nSeu saldo: {this.saldo}" +
+                "\n*************************************************\n");
+        }
+    }
+}
--- Banco/Banco/view/MenuView.cs
+++ Banco/Banco/view/MenuView.cs
@@ -4,19 +4,18 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Banco.Model;
+using Banco.Controller;
 
 namespace Banco.view
 {
     internal class MenuView
     {
-        private static ConsoleKeyInfo consoleKeyInfo;
-        public static void MenuConsole()
-        {   
-            int confirm = 0;
+        public static int confirm = 0;
 
-            bool parada = false;
-            while (parada == false)
-            {
+        public static void MenuConsole()
+        {
+            
                 Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine(
                     "\n ________________________________________" +
@@ -31,7 +30,7 @@ namespace Banco.view
                     "\n******************************************" +
                     "\n|                                        |" +
                     "\n|    O que você deseja para hoje?        |" +
-                    "\n|                                        |"+
+                    "\n|                                        |" +
                     "\n| 1-Criar Conta                          |" +
                     "\n| 2-Listar todas as contas               |" +
                     "\n| 3-Buscar conta Por numero              |" +
@@ -41,91 +40,12 @@ namespace Banco.view
                     "\n| 7-Depositar                            |" +
                     "\n| 8-Transferir valores entre Contas      |" +
                     "\n| 9-Sair                                 |" +
+                    "\n| 0-Visualizar Informações               |" +
                     "\n|                                        |" +
                     "\n******************************************");
                 confirm = Convert.ToInt32(Console.ReadLine());
                 Console.Clear();
-
-                switch (confirm)
-                {
-                    case 1:
-                        Console.ForegroundColor = ConsoleColor.Cyan;
-                        Console.Write("Criar Conta: ");
-                        Console.ResetColor();
-                        KeyPress();
-                        break;
-                    case 2:
-                        Console.ForegroundColor = ConsoleColor.Cyan;
-                        Console.WriteLine("Listar Todas as Contas:\n\n");
-                        Console.ResetColor();
-                        KeyPress();
-                        break;
-                    case 3:
-                        Console.ForegroundColor = ConsoleColor.Cyan;
-                        Console.Write("Digite o numero referente a sua conta: ");
-                        Console.ResetColor();
-                        KeyPress();
-                        break;
-                    case 4:
-                        Console.ForegroundColor = ConsoleColor.Cyan;
-                        Console.Write("Digite o numero referente a sua conta: ");
-                        Console.ResetColor();
-                        KeyPress();
-                        break;
-                    case 5:
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.Write("Apagar a sua conta: ");
-                        Console.ResetColor();
-                        KeyPress();
-                        break;
-                    case 6:
-                        Console.ForegroundColor = ConsoleColor.Blue;
-                        Console.Write("Sacar o Dinheiro da sua conta: ");
-                        Console.ResetColor();
-                        KeyPress();
-                        break;
-                    case 7:
-                        Console.ForegroundColor = ConsoleColor.Blue;
-                        Console.Write("Depositar o Dinheiro da sua conta: ");
-                        Console.ResetColor();
-                        KeyPress();
-                        break;
-                    case 8:
-                        Console.ForegroundColor = ConsoleColor.Blue;
-                        Console.Write("Transferir o Dinheiro da sua conta: ");
-                        Console.ResetColor();
-                        KeyPress();
-                        break;
-                    case 9:
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine(
-                            "\n=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+++=+=+=+=+=+=+=+=+=+=+=" +
-                            "\nBanco Suspeito - Para Sempre no Console de sua casa\n" +
-                            "Cuidado com seu CMD\n");
-                        Sobre();
-                        Console.ResetColor();
-                        System.Environment.Exit(0);
-                        break;
-                }
-            }
-        }
-        static void Sobre()
-        {
-            Console.WriteLine("\n*********************************************************");
-            Console.WriteLine("Projeto Desenvolvido por: ");
-            Console.WriteLine("Mateus Siqueira - mateussiqueirasalomao@gmail.com");
-            Console.WriteLine("github.com/mateusSiqueira2004");
-            Console.WriteLine("*********************************************************");
-
-        }
-
-        static void KeyPress()
-        {
-            do
-            {
-                Console.Write("\nPressione Enter para Continuar...");
-                consoleKeyInfo = Console.ReadKey();
-            } while (consoleKeyInfo.Key != ConsoleKey.Enter);
+                ControllerConta.controllerConta();
         }
     }
 }
